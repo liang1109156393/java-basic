9fc3d1635048afae5fe5e5623977f8d9f6def4ea
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-12-27 20:14:26 +0800

    thread实现方式2

--- src/main/java/com/basic/design/proxy/cglib/RoleDao.java
+++ src/main/java/com/basic/design/proxy/cglib/RoleDao.java
@@ -7,6 +7,6 @@ package com.basic.design.proxy.cglib;
  **/
 public class RoleDao {
     public  void save() {
-        System.out.println("----已经保存数据!----");
+        System.out.println("----已经保存数据!----   ");
     }
 }
